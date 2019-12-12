49c4628a8e529880a79db3478a647fb15c065e35
Houtrry <houtrry@hotmail.com>
2019-12-13 00:15:11 +0800

    完善有向无环拓扑图排序算法

--- appinitdispatcher/src/main/java/com/houtrry/appinitdispatcher/AppInitDispatcher.kt
+++ appinitdispatcher/src/main/java/com/houtrry/appinitdispatcher/AppInitDispatcher.kt
@@ -1,22 +1,27 @@
 package com.houtrry.appinitdispatcher
 
+import java.util.*
+
 /**
  * @author: houtrry
  * @time: 2019/12/12
  * @desc:
  */
-class AppInitDispatcher private constructor(){
+class AppInitDispatcher private constructor() {
+
+    private val tasks: MutableList<Task> = ArrayList()
 
     companion object {
-        val INSTANCE : AppInitDispatcher by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) { AppInitDispatcher() }
+        val INSTANCE: AppInitDispatcher by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) { AppInitDispatcher() }
     }
 
     fun addTask(task: Task): AppInitDispatcher {
-
+        tasks.add(task)
         return this
     }
 
     fun start(): AppInitDispatcher {
+        val directedAcyclicGraphSort = directedAcyclicGraphSort(tasks)
         return this
     }
 }
---
+++ appinitdispatcher/src/main/java/com/houtrry/appinitdispatcher/Utils.kt
@@ -0,0 +1,11 @@
+package com.houtrry.appinitdispatcher
+
+/**
+ * @author: houtrry
+ * @time: 2019/12/13
+ * @desc:
+ */
+fun directedAcyclicGraphSort(sources:MutableList<Task>) :List<Task>{
+
+    return ArrayList()
+}
